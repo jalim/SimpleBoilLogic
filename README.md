1ae516f366f96bf25d01147a64f80e21d32f87c3
Luke Mullan <lmullan@royston.com.au>
2017-06-16 17:21:57 +1000

    First Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# craftbeerpiboil
+Boil logic for craftbeerpi
+
+Simple logic to set the heater power setting during the ramp up period and then have it scaled back to a boiling power setting.
+
+For example 100% ramp up power would be used until the target of say 98°C is reached after which the heater power would be reduced to the desired setting for a rolling boil ie 70%.
---
+++ __init__.py
@@ -0,0 +1,28 @@
+import logging
+import time
+
+from modules import cbpi
+from modules.core.controller import KettleController
+from modules.core.props import Property
+
+@cbpi.controller
+class BoilLogic(KettleController):
+
+    ramp_power = Property.Number("Ramp Up Power %", True, 100)
+    boil_power = Property.Number("Boil Power %", True, 70)
+
+    def run(self):
+
+        r_power = int(self.ramp_power)
+        r_limit = self.get_target_temp()
+        b_power = int(self.boil_power)
+
+
+        while self.is_running():
+            temp = self.get_temp()
+            if temp < r_limit:
+                self.heater_on(r_power)
+            if temp >= r_limit:
+                self.heater_on(b_power)
+            self.sleep(5)
+            self.heater_off()
