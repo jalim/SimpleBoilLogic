b1e8498ef8ec5152d3efb8933ff8c43f722d78d2
Luke Mullan <lmullan@royston.com.au>
2017-06-17 19:52:57 +1000

    Add stop command

--- __init__.py
+++ __init__.py
@@ -1,9 +1,7 @@
-import logging
-import time
-
 from modules import cbpi
 from modules.core.controller import KettleController
 from modules.core.props import Property
+
 
 @cbpi.controller
 class SimpleBoilLogic(KettleController):
@@ -11,15 +9,20 @@ class SimpleBoilLogic(KettleController):
     ramp_power = Property.Number("Ramp Up Power %", True, 100)
     boil_power = Property.Number("Boil Power %", True, 70)
 
+    def stop(self):
+
+        super(KettleController, self).stop()
+        self.heater_off()
+
     def run(self):
 
         r_power = int(self.ramp_power)
-        r_limit = self.get_target_temp()
         b_power = int(self.boil_power)
 
 
         while self.is_running():
             temp = self.get_temp()
+            r_limit = self.get_target_temp()
             if temp < r_limit:
                 self.heater_on(r_power)
             if temp >= r_limit:
