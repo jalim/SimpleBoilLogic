7721ceec1dec5c25c3f0d62968a03d09b58c7444
Luke Mullan <lmullan@royston.com.au>
2017-06-19 18:36:30 +1000

    Minor fix to ramp power

--- __init__.py
+++ __init__.py
@@ -27,7 +27,7 @@ class SimpleBoilLogic(KettleController):
             r_limit = self.get_target_temp()
             if temp < r_limit:
                 self.heater_on(r_power)
-                self.actor_power(b_power)
+                self.actor_power(r_power)
             if temp >= r_limit:
                 self.heater_on(b_power)
                 self.actor_power(b_power)
